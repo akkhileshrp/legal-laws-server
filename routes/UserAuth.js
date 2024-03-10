e25448bda61c3f28fcd0919c342d013c4cd63366
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-03-10 18:14:26 +0530

    Login completed using jwt

--- routes/UserAuth.js
+++ routes/UserAuth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const UserSchema = require('../models/userSchema');
 const ErrorHandler = require('../middleware/errorMiddleware');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+require('dotenv').config();
 
 router.post("/register", async (req, res, next) => {
     try {
@@ -14,6 +17,27 @@ router.post("/register", async (req, res, next) => {
         const newUser = new UserSchema({ firstName, lastName, email, phoneNumber, password });
         await newUser.save();
         res.status(201).send({ message: "User registered successfully" });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserSchema.findOne({ email: email });
+        if (!user) {
+            res.status(400).send({ message: "Incorrect username and password" });
+            return;
+        }
+        const isUser = await bcrypt.compare(password, user.password);
+        if (!isUser) {
+            res.status(400).send({ message: "Invalid credentials" });
+            return;
+        }
+        const authToken = jwt.sign({ userId: user._id }, process.env.AUTH_SECRET, { expiresIn: "20m" });
+        res.cookie("authToken", authToken, { httpOnly: true });
+        res.status(200).send({ message: "Login successful", token: authToken });
     } catch (error) {
         next(error);
     }
