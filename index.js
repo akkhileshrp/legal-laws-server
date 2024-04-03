75c09457b54274207665c97bc28e55883aee65ed
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-04-03 13:30:51 +0530

    port and routes handled

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const app = express();
 const userAuth = require('./routes/UserAuth');
 
+app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser());
 app.use('/auth', userAuth);
 
